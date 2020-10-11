d8aa7364e068387f76e1f517e624c0efd2aa83db
thiagoboeker <thiago2kboeker@gmail.com>
2020-10-11 13:06:02 -0300

    correcao getnet endpoint

--- lib/noxi/getnet/getnet.ex
+++ lib/noxi/getnet/getnet.ex
@@ -2,7 +2,7 @@ defmodule Noxi.Getnet do
   alias Noxi.Getnet.AuthService
   alias Noxi.CreditCardModel
 
-  @getnet_endpoint Application.fetch_env!(:noxi, :getnet_endpoint)
+  @getnet_endpoint "https://api-sandbox.getnet.com.br"
 
   defp post(url, data, token) do
     HTTPoison.post(@getnet_endpoint <> url, Jason.encode!(data), [

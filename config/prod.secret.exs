7ba1ec636006a12fe87820a195315944d74e1b6e
thiagoboeker <thiago2kboeker@gmail.com>
2020-10-11 13:09:23 -0300

    agora vai

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -27,6 +27,14 @@ config :noxi, NoxiWeb.Endpoint,
   http: [:inet6, port: String.to_integer(System.get_env("PORT") || "4000")],
   secret_key_base: secret_key_base
 
+config :noxi, :firebase_api_key, "AIzaSyBkmp2bZUTuTi4K6Z_JQywboZXJrVyLLsI"
+
+config :noxi, :client_id, "93f984b1-8ba9-4895-b7b3-08f9a1032085"
+
+config :noxi, :getnet_endpoint, "https://api-sandbox.getnet.com.br"
+
+config :noxi, :client_secret, "5cc8032c-a321-44cf-ab51-d843fbd14d3b"
+
 # ## Using releases (Elixir v1.9+)
 #
 # If you are doing OTP releases, you need to instruct Phoenix
